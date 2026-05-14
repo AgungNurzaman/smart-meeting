ede82d5571c37c9a1b4b310f429a0f0cf38cb46c
unknown <clion9030@gmail.com>
2026-05-14 23:27:38 +0700

    update full script js

--- www/smart-meeting/assets/js/script.js
+++ www/smart-meeting/assets/js/script.js
@@ -1,2 +1,104 @@
-// TEST JS
-console.log("script.js berhasil");
+// FILTER TANGGAL
+
+document
+.getElementById(
+'filterTanggal'
+)
+
+.addEventListener(
+'change',
+function(){
+
+let tanggal =
+this.value;
+
+let rows =
+document.querySelectorAll(
+'#tableBooking tbody tr'
+);
+
+rows.forEach(function(row){
+
+let isiTanggal =
+row.cells[1].innerText;
+
+if(
+tanggal == ''
+||
+isiTanggal == tanggal
+){
+
+row.style.display='';
+
+}else{
+
+row.style.display='none';
+
+}
+
+});
+
+});
+
+// CARI RUANGAN
+
+document
+.getElementById(
+'cariRuangan'
+)
+
+.addEventListener(
+'keyup',
+function(){
+
+let keyword =
+this.value.toLowerCase();
+
+let rows =
+document.querySelectorAll(
+'#tableBooking tbody tr'
+);
+
+rows.forEach(function(row){
+
+let namaRuangan =
+row.cells[3]
+.innerText
+.toLowerCase();
+
+if(
+namaRuangan.includes(keyword)
+){
+
+row.style.display='';
+
+}else{
+
+row.style.display='none';
+
+}
+
+});
+
+});
+
+// HAPUS DATA
+
+function hapusData(id){
+
+let kode = prompt(
+'Ketik HAPUS'
+);
+
+if(kode == 'HAPUS'){
+
+window.location =
+'hapus.php?id='+id;
+
+}else{
+
+alert('Kode salah');
+
+}
+
+}
